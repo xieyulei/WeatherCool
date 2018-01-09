91cc9236cc8396c39f91cca9371732ad1a998929
xieyulei <940982168@qq.com>
2018-01-09 10:23:58 +0800

    添加编码日志类，用于记录编码逻辑；添加项目相关依赖；

--- app/src/main/java/com/xyl/weathercool/MainActivity.java
+++ app/src/main/java/com/xyl/weathercool/MainActivity.java
@@ -3,6 +3,13 @@ package com.xyl.weathercool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  db包用于存放数据库模型相关的代码
+ *  gson包用于GSON模型相关的代码
+ *  service包用于存放服务相关的代码
+ *  util包用于存放工具相关的代码
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
