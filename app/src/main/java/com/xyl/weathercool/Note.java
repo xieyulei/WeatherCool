d13ebb0c04a62caaa6f3ec660f7647b2fc3e6c54
xieyulei <940982168@qq.com>
2018-01-09 10:26:34 +0800

    添加编码日志类

---
+++ app/src/main/java/com/xyl/weathercool/Note.java
@@ -0,0 +1,21 @@
+package com.xyl.weathercool;
+
+/**
+ *  Created by xyl on 2018/1/9.
+ *  This class is used for record writing notes
+ */
+
+public class Note {
+
+    /**
+     * 第一部分：引入外部依赖库
+     *
+     * 依赖库在：app/build.gradle 中进行编辑，在denpendencies闭包中添加四个依赖库，分别为：数据库依赖、网络请求依赖、GSON依赖、Glide依赖；
+     * 数据库依赖： compile 'org.litepal.android:core:1.3.2'
+     * 网络请求依赖：compile 'com.squareup.okhttp3:okhttp:3.4.1'
+     * GSON : 'com.google.code.gson:gson:2.7'
+     * Glide: 'com.github.bumptech.glide:glide:3.7.0'
+     *
+     */
+
+}
